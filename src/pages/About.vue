678d73d8f44bc7ef2c20bd180fa2d06865623311
Milen Radkov <milenradkov@me.com>
2020-04-12 10:12:15 +0300

    update colors, cleanup

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,19 +1,12 @@
 <template>
   <Layout>
     <h1 class="mb-5">About</h1>
-    <p class="lead">This is a custom, static page, independent of your WordPress content.</p>
+    <p class="lead">hack - blockchain & dlt solutions</p>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi,
-      eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores
-      inventore iste reprehenderit maxime! Iusto.
-    </p>
-    <g-image src="~/assets/images/sampleimage.jpg" alt class="img-fluid mb-3" />
-    <p>
-      Maecenas sed diam eget risus varius blandit sit amet non magna. Vestibulum id ligula porta
-      felis euismod semper. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur.
-      Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis
-      consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor
-      ligula, eget lacinia odio sem nec elit.
+      hack.bg ltd. is a software development company specializing in blockchain & DLT technology
+      solutions. We are experts in developing distributed applications (dapps). Being part of the
+      technological revolution is allowing us to build the technologies of the future. Our team is
+      passionate, motivated, fully distributed international and fast growing.
     </p>
   </Layout>
 </template>
