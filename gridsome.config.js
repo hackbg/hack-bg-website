36f12e7b20ca857c3f4e1401e6e50d8682cdf7f2
Milen Radkov <milenradkov@me.com>
2020-04-12 08:34:19 +0300

    updated gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,45 +1,46 @@
-const path = require("path");
+const path = require('path')
 
 function addStyleResource(rule) {
   rule
-    .use("style-resource")
-    .loader("style-resources-loader")
+    .use('style-resource')
+    .loader('style-resources-loader')
     .options({
-      patterns: [path.resolve(__dirname, "./src/assets/style/*.scss")]
-    });
+      patterns: [path.resolve(__dirname, './src/assets/style/*.scss')],
+    })
 }
 
 module.exports = {
-  siteName: "hack",
-  siteDescription: "Blockchain & DLT Solutions",
+  siteName: 'hack',
+  siteDescription: 'Blockchain & DLT Solutions',
   plugins: [
     {
-      use: "~/src/plugins/wp-source/",
+      use: '~/src/plugins/wp-source/',
       options: {
-        baseUrl: "https://hack.bg", // required - Replace me with your Wordpress URL
-        typeName: "WordPress", // GraphQL schema name (Optional)
+        baseUrl: 'https://hack.bg', // required - Replace me with your Wordpress URL
+        typeName: 'WordPress', // GraphQL schema name (Optional)
         perPage: 100, // How many posts to load from server per request (Optional)
         concurrent: 10, // How many requests to run simultaneously (Optional)
         routes: {
           WordpressPage: node => {
-            return new URL(node.link).pathname;
+            return new URL(node.link).pathname
           },
-          post: "/blog/:slug", //adds route for "post" post type (Optional)
-          post_tag: "/tag/:slug" // adds route for "post_tag" post type (Optional)
+          WordpressPost: '/:category/:slug', //adds route for "post" post type (Optional)
+          WordpressCategory: '/:slug', //adds route for "post" post type (Optional)
+          post_tag: '/tag/:slug', // adds route for "post_tag" post type (Optional)
         },
         createPages: {
-          approach: "exclude", // include or exclude, default is include
-          list: [] //an array of page slugs to include or exclude, ex. ['about', 'our-team'], default is an empty array
-        }
-      }
-    }
+          approach: 'exclude', // include or exclude, default is include
+          list: [], //an array of page slugs to include or exclude, ex. ['about', 'our-team'], default is an empty array
+        },
+      },
+    },
   ],
   chainWebpack(config) {
     // Load variables for all vue-files
-    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
 
     types.forEach(type => {
-      addStyleResource(config.module.rule("scss").oneOf(type));
-    });
-  }
-};
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  },
+}
