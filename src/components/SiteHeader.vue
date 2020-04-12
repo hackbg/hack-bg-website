fea4c4253633401cbda182063908f48ccbfc3e73
Milen Radkov <milenradkov@me.com>
2020-04-12 17:46:07 +0300

    restructure, separate landing page

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="py-4 py-sm-5">
+    <b-navbar class="p-0" toggleable="lg" type="light">
+      <b-navbar-brand>
+        <b-navbar-brand class="font-family-headings" to="/">
+          <span class="h3 text-dark">hack</span>
+        </b-navbar-brand>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto ">
+          <b-nav-item to="/" exact-active-class="active">Home</b-nav-item>
+          <b-nav-item to="/about" active-class="active">About</b-nav-item>
+          <b-nav-item to="/blog" active-class="active">Blog</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </header>
+</template>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,22 @@
 <template>
   <Layout>
-    <h1 class="mb-5">About</h1>
-    <p class="lead">hack - blockchain & dlt solutions</p>
-    <p>
-      hack.bg ltd. is a software development company specializing in blockchain & DLT technology
-      solutions. We are experts in developing distributed applications (dapps). Being part of the
-      technological revolution is allowing us to build the technologies of the future. Our team is
-      passionate, motivated, fully distributed international and fast growing.
-    </p>
+    <b-row class="align-items-start">
+      <transition name="fade" appear>
+        <b-col tag="main" cols="12" lg="12" class="bg-white p-0 rounded shadow-lg">
+          <div class="overflow-hidden p-4 p-sm-5 ">
+            <h1 class="mb-5">About</h1>
+            <p class="lead">hack - blockchain & dlt solutions</p>
+            <p>
+              hack.bg ltd. is a software development company specializing in blockchain & DLT
+              technology solutions. We are experts in developing distributed applications (dapps).
+              Being part of the technological revolution is allowing us to build the technologies of
+              the future. Our team is passionate, motivated, fully distributed international and
+              fast growing.
+            </p>
+          </div>
+        </b-col>
+      </transition>
+    </b-row>
   </Layout>
 </template>
 
