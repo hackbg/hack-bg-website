67432fc7a9d2ccab39834292441735dde1fcf29c
Milen Radkov <milenradkov@me.com>
2020-04-28 17:08:35 +0300

    move styles to global scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 // bootstrap
 import BootstrapVue from 'bootstrap-vue'
-import '~/assets/style/style.css'
 //global stylesheet
 import '~/assets/style/styles.scss'
 import DefaultLayout from '~/layouts/Default.vue'
