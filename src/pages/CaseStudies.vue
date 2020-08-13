8473fa8155df13c7f238262433a528523fa6554a
Milen Radkov <milenradkov@me.com>
2020-08-13 17:33:46 +0300

    create case studies page

---
+++ src/pages/CaseStudies.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <b-row class="align-items-start">
+      <transition name="fade" appear>
+        <b-col tag="main" cols="12" lg="12" class="p-0 rounded">
+          <div class="overflow-hidden p-4 p-sm-5 ">
+            <h1 class="mb-5">Case Studies</h1>
+            <p>Case studies to be listed here</p>
+          </div>
+        </b-col>
+      </transition>
+    </b-row>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Case studies',
+  },
+};
+</script>
