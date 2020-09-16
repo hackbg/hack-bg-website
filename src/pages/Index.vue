ab3fd31784e9b959f0eb512bc1c2ab78d88adade
Milen Radkov <milenradkov@me.com>
2020-09-16 08:42:54 +0300

    add description to landing page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,16 +10,18 @@
 </template>
 
 <script>
-import About from './../components/landing/About'
-import Services from './../components/landing/Services'
-import Contact from './../components/landing/Contact'
-import Home from './../components/landing/Home'
-import Partners from './../components/landing/Partners'
-import Projects from './../components/landing/Projects'
+import About from './../components/landing/About';
+import Services from './../components/landing/Services';
+import Contact from './../components/landing/Contact';
+import Home from './../components/landing/Home';
+import Partners from './../components/landing/Partners';
+import Projects from './../components/landing/Projects';
 
 export default {
   metaInfo: {
     title: 'Blockchain Development & Consulting',
+    desciption:
+      'Hack is a pioneer in Blockchain, providing services from Smart Contracts Development and Security Audit for ICO and STO Tokensales to fully-fledged Decentralized Applications.',
   },
   components: {
     Services,
@@ -29,5 +31,5 @@ export default {
     Partners,
     Projects,
   },
-}
+};
 </script>
